4310964d05d6ab341cbd4561736fe6e489058b8a
Polokniko <fizzyinc3@gmail.com>
2017-12-08 21:35:56 -0500

    update

--- generators.py
+++ generators.py
@@ -1,23 +1,23 @@
 from random import randint
 
-with open('allwords.txt', 'r') as words:
-	allwordslist = words.read().split('\n')
+with open('easynouns.txt', 'r') as words:
+	easywordslist = words.read().split('\n')
 
 def genpass(length):
-     """
-     Will generate a random string of the given length of words seperated by spaces
-     """
-     password = ''
-     for i in range(length):
-         password += allwordslist[randint(0, 58110)] + ' '
-     return password.strip()
+    """
+    Will generate a random string of the given length of words seperated by spaces
+    """
+    password = ''
+    for i in range(length):
+        password += easywordslist[randint(0, 310)] + ' '
+    return password.strip()
 
 def genstr(length):
-     """
-     Will generate a random string of ASCII characters with the length of the given value
-     """
-	word = ''
-	chars = [randint(0, 93) for i in range(length)]
-	for i in chars:
-		word += """`1234567890-=qwertyuiop[]\asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM<>? """[i]
-	return word
+    """
+    Will generate a random string of ASCII characters with the length of the given value
+    """
+    word = ''
+    chars = [randint(0, 93) for i in range(length)]
+    for i in chars:
+    	word += """`1234567890-=qwertyuiop[]\asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM<>? """[i]
+    return word
